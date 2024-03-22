05e466448f7005db3eab7bb7e62229cc68dbf953
amr9 <amr12201148@yahoo.com>
2024-03-22 15:35:55 -0700

    fixing packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 #!/usr/bin/pup
-# Install an specific version of flask (2.1.0)
+# Install flask (2.1.0)
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'werkzeug' :
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
